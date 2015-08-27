2f743d309f2cc9b9f672187025b8d773a4fcf7ed
Ken Torry <kentorry@afrodesiamedia.com>
2015-08-27 18:19:45 -0500

    finished up the afroFramework up to a certain function point

--- afro-ext/afroFramework/afroFrameworkDirective.js
+++ afro-ext/afroFramework/afroFrameworkDirective.js
@@ -4,7 +4,8 @@ angular.module("afroFramework").directive("afroFramework", function () {
     return {
         transclude: false,
         scope: {
-
+            title: "@",
+            iconFile: "@"
         },
         controller: "afroFrameworkController",
         templateUrl: "afro-ext/afroFramework/afroFrameworkTemplate.html"
